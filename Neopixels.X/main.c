8033dafcf2e038cbd0f6f00098e70fb492d9a414
AgentD1 <jacobandian3@gmail.com>
2021-12-08 10:52:48 -0500

    Update main.c

--- Neopixels.X/main.c
+++ Neopixels.X/main.c
@@ -20,16 +20,17 @@
 #include    "Neopixel.h"
 
 void rainbowCycle();
+void solidColour();
+void pulsingSolidColour();
 
 void hsvtorgb(unsigned char *r, unsigned char *g, unsigned char *b, unsigned char h, unsigned char s, unsigned char v);
 
-int numFunctions = 2;
-void (*colorFunctions[])(void) = {rainbowCycle};
+unsigned char numFunctions = 3;
+unsigned char functionIndex = 0;
+void (*colorFunctions[])(void) = { rainbowCycle, solidColour, pulsingSolidColour };
 
 int tick = 1;
 int ticks_left = 0;
-
-unsigned char left_shift = 0;
 
 unsigned char reds[45];
 unsigned char greens[45];
@@ -48,76 +49,161 @@ int main(void) {
     while(1) {
         neopixel_fill_a(45, reds, greens, blues);
         
-        if(ticks_left == 0) {
-            if(SW2 == 0 && left_shift != 8) {
-                left_shift++;
-                ticks_left = 20;
-            }
-            if(SW3 == 0 && left_shift != 0) {
-                left_shift--;
-                ticks_left = 20;
-            }
-        } else {
-            ticks_left--;
+        if(SW1 == 0 && ticks_left == 0) {
+            functionIndex++;
+            functionIndex %= numFunctions;
+            
+            ticks_left = 20;
         }
         
-        __delay_ms(4);
+        (*colorFunctions[functionIndex])();
         
-        for(unsigned char i = 0; i < 45; i++) {
-            hsvtorgb(&reds[i], &greens[i], &blues[i], (unsigned char)(tick * 2 + 1) + (i * 2), 255, 255);
-            reds[i] >>= left_shift;
-            greens[i] >>= left_shift;
-            blues[i] >>= left_shift;
-        }
+        if(ticks_left != 0) ticks_left--;
         
-        tick += 1;
-                
+        tick++;
+        
         // Activate bootloader if SW1 is pressed.
-        if(SW1 == 0) {
+        if(SW1 == 0 && SW2 == 0) {
             RESET();
         }
     }
 }
 
+bool reversed = false;
+unsigned char left_shift = 0;
+
 void rainbowCycle() {
-    
+    if(ticks_left == 0) {
+        if(SW2 == 0 && left_shift != 8) {
+            left_shift++;
+            ticks_left = 20;
+        }
+        if(SW3 == 0 && left_shift != 0) {
+            left_shift--;
+            ticks_left = 20;
+        }
+        if(SW4 == 0) {
+            reversed = !reversed;
+            ticks_left = 20;
+        }
+    }
+
+    __delay_ms(4);
+
+    for(unsigned char i = 0; i < 45; i++) {
+        if(reversed) {
+            hsvtorgb(&reds[i], &greens[i], &blues[i], (unsigned char)(-tick) + (i * 2), 255, 255);
+        } else {
+            hsvtorgb(&reds[i], &greens[i], &blues[i], (unsigned char)(tick) + (i * 2), 255, 255);
+        }
+        reds[i] >>= left_shift;
+        greens[i] >>= left_shift;
+        blues[i] >>= left_shift;
+    }
 }
 
-// http://web.mit.edu/storborg/Public/hsvtorgb.c
-void hsvtorgb(unsigned char *r, unsigned char *g, unsigned char *b, unsigned char h, unsigned char s, unsigned char v) {
-    unsigned char region, fpart, p, q, t;
-    
-    if(s == 0) {
-        /* color is grayscale */
-        *r = *g = *b = v;
-        return;
+unsigned char hue = 1;
+
+void solidColour() {
+    if(ticks_left == 0) {
+        if(SW2 == 0 && left_shift != 8) {
+            left_shift++;
+            ticks_left = 20;
+        }
+        if(SW3 == 0 && left_shift != 0) {
+            left_shift--;
+            ticks_left = 20;
+        }
+        if(SW4 == 0) {
+            hue += 4;
+            ticks_left = 20;
+        }
+        if(SW5 == 0) {
+            hue -= 4;
+            ticks_left = 20;
+        }
     }
-    
-    /* make hue 0-5 */
-    region = h / 43;
-    /* find remainder part, make it from 0-255 */
-    fpart = (h - (region * 43)) * 6;
-    
-    /* calculate temp vars, doing integer multiplication */
-    p = (v * (255 - s)) >> 8;
-    q = (v * (255 - ((s * fpart) >> 8))) >> 8;
-    t = (v * (255 - ((s * (255 - fpart)) >> 8))) >> 8;
+
+    __delay_ms(4);
+
+    for(unsigned char i = 0; i < 45; i++) {
+        hsvtorgb(&reds[i], &greens[i], &blues[i], hue, 255, 255);
         
-    /* assign temp vars based on color cone region */
-    switch(region) {
-        case 0:
-            *r = v; *g = t; *b = p; break;
-        case 1:
-            *r = q; *g = v; *b = p; break;
-        case 2:
-            *r = p; *g = v; *b = t; break;
-        case 3:
-            *r = p; *g = q; *b = v; break;
-        case 4:
-            *r = t; *g = p; *b = v; break;
-        default:
-            *r = v; *g = p; *b = q; break;
+        reds[i] >>= left_shift;
+        greens[i] >>= left_shift;
+        blues[i] >>= left_shift;
     }
-    
-    return;
 }
+
+bool pulsingReversed = false;
+
+void pulsingSolidColour() {
+    if(ticks_left == 0) {
+        if(SW2 == 0 && left_shift != 8) {
+            left_shift++;
+            ticks_left = 20;
+        }
+        if(SW3 == 0 && left_shift != 0) {
+            left_shift--;
+            ticks_left = 20;
+        }
+        if(SW4 == 0) {
+            hue += 4;
+            ticks_left = 20;
+        }
+        if(SW5 == 0) {
+            hue -= 4;
+            ticks_left = 20;
+        }
+    }
+
+    __delay_ms(4);
+
+    for(unsigned char i = 0; i < 45; i++) {
+        hsvtorgb(&reds[i], &greens[i], &blues[i], hue, 255, tick);
+        
+        reds[i] >>= left_shift;
+        greens[i] >>= left_shift;
+        blues[i] >>= left_shift;
+    }
+}
+
+
+// http://web.mit.edu/storborg/Public/hsvtorgb.c
+//void hsvtorgb(unsigned char *r, unsigned char *g, unsigned char *b, unsigned char h, unsigned char s, unsigned char v) {
+//    unsigned char region, fpart, p, q, t;
+//    
+//    if(s == 0) {
+//        /* color is grayscale */
+//        *r = *g = *b = v;
+//        return;
+//    }
+//    
+//    /* make hue 0-5 */
+//    region = h / 43;
+//    /* find remainder part, make it from 0-255 */
+//    fpart = (h - (region * 43)) * 6;
+//    
+//    /* calculate temp vars, doing integer multiplication */
+//    p = (v * (255 - s)) >> 8;
+//    q = (v * (255 - ((s * fpart) >> 8))) >> 8;
+//    t = (v * (255 - ((s * (255 - fpart)) >> 8))) >> 8;
+//        
+//    /* assign temp vars based on color cone region */
+//    switch(region) {
+//        case 0:
+//            *r = v; *g = t; *b = p; break;
+//        case 1:
+//            *r = q; *g = v; *b = p; break;
+//        case 2:
+//            *r = p; *g = v; *b = t; break;
+//        case 3:
+//            *r = p; *g = q; *b = v; break;
+//        case 4:
+//            *r = t; *g = p; *b = v; break;
+//        default:
+//            *r = v; *g = p; *b = q; break;
+//    }
+//    
+//    return;
+//}
